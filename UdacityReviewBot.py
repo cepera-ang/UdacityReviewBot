379d1609727e7b804bbfeb6fa8781d764ec40b11
“Serg <cepera.ang@gmail.com>
2016-10-23 21:00:24 +0800

    Added quick and dirty usage of Udacity's grading-assigner.py script + /stop command

--- UdacityReviewBot.py
+++ UdacityReviewBot.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-
+import subprocess
 from telegram.ext import Updater, CommandHandler
 import logging
 
@@ -10,15 +10,24 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
+p = None
 
 def start(bot, update):
     logger.info('Received the /start command')
     update.message.reply_text('Hello World!')
+    global p
+    p = subprocess.Popen(['python', 'grading-assigner.py'])
 
 def hello(bot, update):
     logger.info('Received the /hello command')
     update.message.reply_text(
         'Hello {}'.format(update.message.from_user.first_name))
+
+def stop(bot, update):
+    logger.info('Received the /stop command')
+    update.message.reply_text('Stopping the grader script')
+    global p
+    p.terminate()
 
 def any(bot, update):
     logger.info('Received something strange') # TODO: add information about command received
@@ -34,6 +43,7 @@ updater = Updater(token)
 
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
+updater.dispatcher.add_handler(CommandHandler('stop', stop))
 # updater.dispatcher.add_error_handler(any)
 updater.start_polling()
 updater.idle()
